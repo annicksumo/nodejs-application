2d17111639a71b00ee381913a5f88bb17e35788b
annicksumo <adoguen@gmail.com>
2025-05-05 00:37:58 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -2,17 +2,13 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
   serverUrl: 'http://54.198.239.165:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'admin',
-	    'sonar.password': '6d36ea0d6bbc7e4ab52e71f6c62c63305e2a21ed',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
+  options: {
+    'sonar.projectKey': 'NodeJsLandmarkTechnologies',
+    'sonar.projectName': 'Node JS Application - Sample',
+    'sonar.projectVersion': '1.0',
+    'sonar.sources': '.',
+    'sonar.sourceEncoding': 'UTF-8',
+    'sonar.login': 'TON_TOKEN_PERSONNEL_ICI'
+  },
 }, () => {});
+
